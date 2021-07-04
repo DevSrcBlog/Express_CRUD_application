33f236ea479d5e4e6069f55bda45a3987d16de0f
alamin <alamin@gmail.com>
2021-07-04 22:54:33 +0600

    crud initial knowledge

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const app = express();
 app.use(cookieParser());
 
 // route
-app.get("/", (req, res) => {
-  console.log(req.query);
-
-  console.log(req.cookies); // with cookie parser package
-  console.log(req.headers.cookie); // without cookie perser package
-
-  res.send(`Category: ${req.query.category}, Featured: ${req.query.featured} `);
-});
+app.get("/", (req, res) => {});
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
+
+/**
+ * HTTP Protocol
+ * HTTP Verbs or Methods
+ * - GET (get a list of data or single object of data)
+ * - POST (create new resources)
+ * - PUT (Update -> whole object)
+ * - PATCH (Update -> update specific propertices)
+ * - DELETE (Delete the resource)
+ *
+ * - Resource URI
+ *  - GET (/books)
+ *  - POST (/books)
+ *  - GET a single (/books/bookId)
+ *  - PUT/PATCH (/books/bookId)
+ *  - DELETE (/books/bookId)
+ *
+ */
