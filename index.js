73f3fc79776e54f0f98e938ec7528e035d8f26d8
alamin <alamin@gmail.com>
2021-07-04 22:26:32 +0600

    find cookies from the development server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+var cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
 
 // route
 app.get("/", (req, res) => {
   console.log(req.query);
+
+  console.log(req.cookies);
+
   res.send(`Category: ${req.query.category}, Featured: ${req.query.featured} `);
 });
 
